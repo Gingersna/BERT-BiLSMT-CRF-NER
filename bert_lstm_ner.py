23a068ec3a28de5356d227b4b0f225d7b63f9b56
Gingersna <513335061@qq.com>
2018-11-27 19:10:52 +0800

    adjust some hyperpara

--- bert_lstm_ner.py
+++ bert_lstm_ner.py
@@ -95,7 +95,7 @@ flags.DEFINE_bool("do_eval", True, "Whether to run eval on the dev set.")
 
 flags.DEFINE_bool("do_predict", True, "Whether to run the model in inference mode on the test set.")
 
-flags.DEFINE_integer("train_batch_size", 22, "Total batch size for training.")
+flags.DEFINE_integer("train_batch_size", 16, "Total batch size for training.")
 
 flags.DEFINE_integer("eval_batch_size", 8, "Total batch size for eval.")
 
